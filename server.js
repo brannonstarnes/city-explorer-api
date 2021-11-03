b20487d911e0ca8ab5590549ef3996219425cf5c
Brannon Starnes <brannlee808@gmail.com>
2021-11-02 20:49:31 -0700

    added json and weather server.js

--- server.js
+++ server.js
@@ -3,9 +3,25 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors'); 
+const weather = require('./data/weather.json');
 
 const app = express(); 
 
 app.use(cors());
 
-app.listen(3001, () =>console.log("I'm listening - your server"));
+
+const PORT = process.env.PORT;
+
+//get method takes (route, callbackHandler(req,res)) 
+//the callback always need args of req, res
+app.get('/weather', handleGetWeather);
+
+
+function handleGetWeather(req,res){
+//respond with status 200 "ok" and send the weather.json 
+  res.status(200).send(weather)
+  console.log(req.query.lat)
+}
+
+app.listen(PORT, () =>console.log(`"I'm listening on ${PORT} - your server"`));
+

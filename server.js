a0315071798f20ac0d29138a98fe3e40bb84a57f
Brannon Starnes <brannlee808@gmail.com>
2021-11-03 23:48:07 -0700

    weather forecasts data renders to page

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors'); 
 const axios = require('axios');
-// const weatherJSON = require('./data/weather.json');
 
 const app = express(); 
 
@@ -13,43 +12,27 @@ app.use(cors());
 
 const PORT = process.env.PORT;
 
-//get method takes (route, callbackHandler(req,res)) 
-//the callback always need args of req, res
 app.get('/weather', handleGetWeather);
 app.get('/*', (req,res) => res.status(403).send('Not Found'));
 
 
 async function handleGetWeather(req,res){
 
-const city_name = req.query.city;
-const lat = req.query.lat;
-const lon = req.query.lon;
+  const lat = req.query.lat;
+  const lon = req.query.lon;
 
-console.log('weather route hit')
-  try {
-    const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=3`
+    console.log('weather route hit')
+  const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=3`;
 
-    let res= await axios.get(url);
-    console.log('MADE IT HERE>>>>>>',res.data)
-    const fetchedWeather = res.data;
-
-    if (fetchedWeather) {
-      const forecastData = fetchedWeather.map(city => new WeatherForecast(city));
-      console.log(forecastData);
-      res.status(200).send(forecastData);
-    } else {
-      res.status(404).send('city not found');
-    }
+  let response= await axios.get(url);
+    
+  let fetchedWeather = response.data.data;
+  let forecastData = fetchedWeather.map(dailyWeather => new WeatherForecast(dailyWeather));
+     console.log(forecastData);
+  res.status(200).send(forecastData);
+     console.log("SENT");  
+};
   
-  } catch(e){
-    res.status(500).send('server error')
-  }
-
-
-  //respond with status 200 "ok" and send the weather.json 
-  res.status(200).send(res.data)
-  
-}
 
 class WeatherForecast {
   constructor(obj){
